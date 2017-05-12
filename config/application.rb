b4ddeeabbff726aa24d11a82687a6fe5dc23af30
joshhaws <joshhaws0@gmail.com>
2017-05-12 13:31:24 -0600

    stuff

--- config/application.rb
+++ config/application.rb
@@ -22,10 +22,10 @@ module GratitudeMoment
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    # Twilio.configure do |config|
-    #   config.account_sid = Rails.application.secrets.twilio_account_sid
-    #   config.auth_token = Rails.application.secrets.twilio_auth_token
-    # end
+    Twilio.configure do |config|
+      config.account_sid = Rails.application.secrets.twilio_account_sid
+      config.auth_token = Rails.application.secrets.twilio_auth_token
+    end
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
