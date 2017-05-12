6835bcea76d6cb3720662a5cf736ddd0d47b77d4
joshhaws <joshhaws0@gmail.com>
2017-05-12 14:14:06 -0600

    stuff

--- app/controllers/moments_controller.rb
+++ app/controllers/moments_controller.rb
@@ -41,7 +41,7 @@ class MomentsController < ApplicationController
   end
 
   def process_sms
-    moment = Moment.create(body: params[:Body].strip, phone: params[:From], twilio_id: params[:MessageSid])
+    Moment.create(body: params[:Body].strip, phone: params[:From], twilio_id: params[:MessageSid])
     respond_to do |format|
       format.xml  { head :ok }
       format.json { head :ok }
